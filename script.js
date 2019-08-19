981abb0651acf42b328079acfc21157ccc07ecd1
erin0418 <erinf0418@gmail.com>
2019-08-18 20:57:26 -0400

    canvas created

---
+++ script.js
@@ -0,0 +1,6 @@
+// sets up the HTML canvas
+const canvas = document.getElementById('canvas');
+const context = canvas.getContext('2d');
+context.fillStyle = '#000';
+context.fillRect(0, 0, 400, 600);
+
